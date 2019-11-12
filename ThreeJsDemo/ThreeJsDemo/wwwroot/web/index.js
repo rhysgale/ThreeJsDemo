ce167b27b5951a9beacb6996d4002680b58c8486
Rhys <rhys.gale@symphonyretailai.com>
2019-11-12 20:42:56 +0000

    Rotating cube + Some lines!

--- ThreeJsDemo/ThreeJsDemo/wwwroot/web/index.js
+++ ThreeJsDemo/ThreeJsDemo/wwwroot/web/index.js
@@ -1,6 +1,37 @@
 ﻿var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500); //FOV, ApectRatio, Near Clipping Plane, Far Clipping Plane
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight); //Width and Height of the window
 document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.set(0, 0, 10);
+
+var lineMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff });
+var lineGeometry = new THREE.Geometry();
+lineGeometry.vertices.push(new THREE.Vector3(-10, 0, 0));
+lineGeometry.vertices.push(new THREE.Vector3(0, 10, 0));
+lineGeometry.vertices.push(new THREE.Vector3(10, 0, 0));
+
+var line = new THREE.Line(lineGeometry, lineMaterial);
+scene.add(line);
+
+// Create lights
+var light = new THREE.PointLight(0xEEEEEE);
+light.position.set(0, 0, -20);
+scene.add(light);
+
+function animate() {
+	requestAnimationFrame(animate);
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render(scene, camera);
+}
+animate();
