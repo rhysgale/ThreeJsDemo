436b756b4a7dbc065c248b26b868524a598d4316
Rhys Gale <rhys.gale@blinds-2go.co.uk>
2019-11-19 12:59:57 +0000

    Projectiles with movement

--- ThreeJsDemo/ThreeJsDemo/wwwroot/web/index.js
+++ ThreeJsDemo/ThreeJsDemo/wwwroot/web/index.js
@@ -7,6 +7,19 @@ var camera;
 var mainCube;
 var pressedKeys = [];
 var speed = .1;
+var projectileSpeed = .1;
+
+var projectiles = [];
+
+function Fire() {
+    var geometry = new THREE.BoxGeometry(0.05, 0.1, 0.05);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var projectile = new THREE.Mesh(geometry, material);
+    projectile.position.set(mainCube.position.x, mainCube.position.y, mainCube.position.z);
+
+    projectiles.push(projectile);
+    scene.add(projectile);
+}
 
 function Startup() {
     scene = new THREE.Scene();
@@ -16,13 +29,19 @@ function Startup() {
     renderer.setSize(window.innerWidth, window.innerHeight); //Width and Height of the window
     document.body.appendChild(renderer.domElement);
 
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var geometry = new THREE.BoxGeometry(1,1,1);
     var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     mainCube = new THREE.Mesh(geometry, material);
-    mainCube.position.set(0, -3, 0)
+    mainCube.position.set(0, -3, 0);
     scene.add(mainCube);
 
     camera.position.set(0, 0, 10);
+}
+
+function ProjectileMovement() {
+    projectiles.forEach(function (projectile) {
+        projectile.position.set(projectile.position.x, projectile.position.y + (1 * projectileSpeed), projectile.position.z);
+    });
 }
 
 //Update loop
@@ -33,11 +52,15 @@ function Update() {
     if (pressedKeys["D".charCodeAt(0)] && pressedKeys["D".charCodeAt(0)] === true) {
         mainCube.position.x += 1 * speed;
     }
+    if (pressedKeys["W".charCodeAt(0)] && pressedKeys["W".charCodeAt(0)] === true) {
+        Fire();
+    }
 }
 
 function animate() {
     requestAnimationFrame(animate);
     Update();
+    ProjectileMovement();
     renderer.render(scene, camera);
 }
 Startup();
